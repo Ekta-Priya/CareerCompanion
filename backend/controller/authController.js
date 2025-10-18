8a9868a1c290a57c64c9b83e00c6898dfe28185b
Ekta-Priya <ektapriya36@gmail.com>
2025-10-19 01:42:03 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -22,8 +22,8 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -49,8 +49,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 

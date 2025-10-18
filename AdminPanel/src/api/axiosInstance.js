1fe52048b3fdfd7bb5d172f932e7b598dafc596e
Ekta-Priya <ektapriya36@gmail.com>
2025-10-19 00:56:54 +0530

    Update axiosInstance.js

--- AdminPanel/src/api/axiosInstance.js
+++ AdminPanel/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "https://careercompanion-backend-mgbo.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
